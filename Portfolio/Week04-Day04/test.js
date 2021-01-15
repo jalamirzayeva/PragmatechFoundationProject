d89ad719fba004745fa5e4306af22dc9d2fdedd5
Jala Mirzayeva <mirzayevajala@gmail.com+>
2021-01-15 16:10:10 +0400

    js ilk test

---
+++ Portfolio/Week04-Day04/test.js
@@ -0,0 +1,35 @@
+let myName="Jale";
+    console.log(myName)
+    
+    
+
+    surname="Mirzayeva";
+    console.log(surname)
+  
+
+
+    age=18;
+    console.log(age)
+    
+
+
+    university="Baku State University";
+    console.log(university)
+    
+
+
+    department="Computer Science";
+    console.log(department)
+)
+
+
+    course=2;
+    console.log(course)
+   
+
+  
+    
+
+
+
+    

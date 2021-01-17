a2f810bc02e7125221b89ae2401bd9f15db44fc5
Jala Mirzayeva <mirzayevajala@gmail.com+>
2021-01-17 20:56:44 +0400

    if/else/else if

---
+++ Portfolio/Week04-Day05/app.js
@@ -0,0 +1,20 @@
+var username="Jaja";
+var password= 2805;
+
+
+
+if(username=="Jale Mirzayeva"){
+    document.write("İstifadəçi adı düzgündür!");
+}else if(username=="Jale"){
+    document.write("İstifadəçi adınızı tam qeyd edin!");
+}else if(username=="Mirzayeva"){
+    document.write("İstifadəçi adınızı tam qeyd edin!");
+}else if(password==20022805){
+    document.write("Parol düzgündür!");
+}else{
+    document.write("Zəhmət olmasa istifadəçi adı və parolu düzgün qeyd edin!")
+}
+   
+
+
+

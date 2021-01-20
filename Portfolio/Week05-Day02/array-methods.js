71a2a72ac2ddd28243d85d23a74bc411659e933b
Jala Mirzayeva <mirzayevajala@gmail.com+>
2021-01-20 22:43:28 +0400

    Methods

--- Portfolio/Week05-Day02/array-methods.js
+++ Portfolio/Week05-Day02/array-methods.js
@@ -27,5 +27,38 @@ function check(yas) {
  let element=yaslar.every(check);
  console.log(element)
 
+//FILL
+let numbers=[20,86,25,75,22,34,200,120,33,89,44];
+console.log(numbers)
+//30 ededini 2-7 araliginde yazdirmaq ucun 
+let doldur=numbers.fill(30,2,8)
+console.log(doldur)
 
- 
+//FILTER
+
+let ages=[20,11,12,26,86]
+function show (age){
+    return age <= 18
+}
+console.log(ages.filter(show))
+
+
+//FIND
+let ededler=[18,11,12,15,25,34,86]
+//20 den kicik olanlari tapmaq isteyrem
+function goster (tap){
+    return tap<20
+}
+let tapilmalilar=ededler.find(goster);
+console.log(tapilmalilar)
+// 1-ci 18 i gosterecek cunki arrayda ilk qarsisina cixan eded 18dir
+
+
+//findIndex
+let massiv=[18,33,34,67,70,90,149,665,9,4,1];
+//Array elementlerinin index nomrelerini tapmaq istesem
+function goster (sira){
+    return sira<35
+}
+let elementnomre=massiv.findIndex(goster)
+console.log(elementnomre)

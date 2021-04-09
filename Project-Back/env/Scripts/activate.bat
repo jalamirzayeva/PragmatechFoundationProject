674b6b1be3df31bf351ec4cc3c3077480856dfb4
Jala Mirzayeva <mirzayevajala@gmail.com+>
2021-04-10 02:30:43 +0400

    Back elave edildi

--- Project-Back/env/Scripts/activate.bat
+++ Project-Back/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\Desktop\squad04\LAYIHE\env
+set VIRTUAL_ENV=C:\Users\Admin\Desktop\Pragmatech\env
 
 if not defined PROMPT set PROMPT=$P$G
 
